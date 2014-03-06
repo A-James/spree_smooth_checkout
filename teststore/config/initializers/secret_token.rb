b505f216cd08410f260a0574d39297039df8e0ed
Matthew Fenelon <matt@fnln.me>
2014-03-06 13:40:43 +0000

    Added a test rails site to manually test the gem with.

---
+++ teststore/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teststore::Application.config.secret_key_base = '994cc5df5f712dc7540e68ceee9db5b494334c4d63996572d0e6855adc7840b4f1401389171d2de867692bb5fddddbb1fa8b40074bf3deddd9bbe3c3b96eaa07'
