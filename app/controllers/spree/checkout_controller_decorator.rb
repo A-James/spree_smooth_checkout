69b47a4c5f8c997ef7e616304387d9843f92f427
Matthew Fenelon <matt@fnln.me>
2014-03-14 11:28:21 +0000

    #2 Fixed redirects used within the checkout controller.
    Redirects were broken by using AJAX to transition through the checkout. This commit overrides the redirect_to method to perform a client side redirect when applicable.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,5 +1,6 @@
 Spree::CheckoutController.module_eval do
   alias_method :update_original, :update
+  alias_method :redirect_to_original, :redirect_to
 
   def update
     # is_summary comes from the form on the summary partial
@@ -31,5 +32,46 @@ Spree::CheckoutController.module_eval do
         format.js { render "summary" }
       end
     end
+  end
+
+  # This method overrides the default redirect_to
+  # to use a clientside redirect when the request
+  # is made using XHR.
+  def redirect_to(*args)
+    redirect_to_original(*args)
+    return unless request.xhr?
+
+    redirect_clientside_when_outside_checkout
+  end
+
+  # Redirects within the checkout path are allowed
+  # because they will be rendered correctly by the
+  # checkout pages.
+  # Redirects outside of the checkout path needs to
+  # break out of the AJAX request to perform the
+  # redirect.
+  def redirect_clientside_when_outside_checkout
+    redirecting_outside_checkout = self.status == 302 && self.location.include?(checkout_path) == false
+    if redirecting_outside_checkout
+      mutate_http_redirect_to_clientside_redirect
+    end
+  end
+
+  def mutate_http_redirect_to_clientside_redirect
+    # record the redirect location before resetting
+    @location = self.location
+
+    reset_http_redirect
+
+    # The redirect template performs a client-side redirect to stop
+    # the browser following the redirects automatically as part of
+    # the AJAX request.
+    render "redirect"
+  end
+
+  def reset_http_redirect
+    self.status = 200
+    headers.delete("Location")
+    self.response_body = nil
   end
 end
