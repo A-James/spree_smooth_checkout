6e9ef4139d04d61e91059d3b574035303c091459
Matthew Fenelon <matt@fnln.me>
2014-03-14 16:16:58 +0000

    #6 Turned off the confirmation checkout screen.
    With the new structure of the checkout process, which shows the contents of the cart at all times, the confirmation screen isn't required.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,6 +3,13 @@ Spree::Order.class_eval do
       after_transition :to => :delivery, :do => :skip_delivery
     end
 
+    # Force the confirmation screen to never appear.
+    # The checkout pages have been modified so that
+    # the customer always knows what they're buying.
+    def confirmation_required?
+      false
+    end
+
     private
       def skip_delivery
         # Progress the state machine to the next state,
