6bc7c1779e1d6f35d02fc290599a1209d5ba74bb
Alex <alex.james@200creative.com>
2014-03-20 11:02:17 +0000

    Update the checkout wrapper to remove links to elsewhere in store

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,6 +1,7 @@
 Spree::CheckoutController.module_eval do
   alias_method :update_original, :update
   alias_method :redirect_to_original, :redirect_to
+  layout '/spree/layouts/spree_checkout'
 
   def update
     # is_summary comes from the form on the summary partial
