e1c7013b112df2d96bd3d0209e4541c9ab18d4ec
Alex <alex.james@200creative.com>
2014-03-21 11:33:47 +0000

    affix the side panel

---
+++ teststore/app/assets/javascripts/store/custom.js
@@ -0,0 +1,12 @@
+$( function() {
+
+  $('#lock-summary')
+
+    .width($('#checkout-summary').width() - 10) //remove 10px for bootstrap negative margins
+
+    .affix({
+      offset: {
+      top: 90 } //height of header
+    });
+
+});
