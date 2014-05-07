5084e0056eef597ed872890b8bf9f76d9d23834f
Alex <alex.james@200creative.com>
2014-05-07 20:39:24 +0100

    pull parts out of test store and into the actual gem ready to refactor

--- app/assets/javascripts/store/spree_smooth_checkout.js
+++ app/assets/javascripts/store/spree_smooth_checkout.js
@@ -1,1 +1,16 @@
-//= require store/spree_frontend
+$( function() {
+
+  $('#lock-summary')
+
+    .width($('#checkout-summary').width() - 10) //remove 10px for bootstrap negative margins
+
+    .affix({
+      offset: {
+      top: 90 } //height of header
+    });
+
+  $('#add-discount-code').on('click', function(){
+    $('#discount-code').removeClass('hidden');
+  });
+
+});
