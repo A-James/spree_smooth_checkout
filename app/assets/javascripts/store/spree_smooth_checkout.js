791d5cae8d853eaa629dcc95767c1d5baabf463a
Alex <alex.james@200creative.com>
2014-05-08 09:18:59 +0100

    layout work

--- app/assets/javascripts/store/spree_smooth_checkout.js
+++ app/assets/javascripts/store/spree_smooth_checkout.js
@@ -1,16 +1,115 @@
+//= require_tree .
+
 $( function() {
 
-  $('#lock-summary')
+  // $('#lock-summary')
 
-    .width($('#checkout-summary').width() - 10) //remove 10px for bootstrap negative margins
+  //   .width($('#checkout-summary').width() - 10) //remove 10px for bootstrap negative margins
 
-    .affix({
-      offset: {
-      top: 90 } //height of header
-    });
+  //   .affix({
+  //     offset: {
+  //     top: 90 } //height of header
+  //   });
 
   $('#add-discount-code').on('click', function(){
     $('#discount-code').removeClass('hidden');
   });
 
 });
+
+
+//------ below line moved in from page, doesn't work here yet
+
+//Only search when value is changed from previous
+//Disable once change clicked
+//Only display a 2 part result
+//Don't use on second form
+//Set drop down selectors to correct values
+//Set all validation / events to trigger on auto fill
+
+// geocoder = new google.maps.Geocoder();
+
+// findAddress = function(postcode) {
+
+//   geocoder.geocode( { 'address': postcode + ", UK" }, function(results, status) {
+
+//     // console.log(results);
+
+//     if (results[0]){
+
+//       try {
+//         var postalTown = $.grep(results[0].address_components, function(el){
+//           return $.inArray('postal_town', el.types) !== -1;
+//         })[0].long_name;
+//       } catch(e) { false }
+//       try {
+//         var locality = $.grep(results[0].address_components, function(el){
+//           return $.inArray('locality', el.types) !== -1;
+//         })[0].long_name;
+//       } catch(e) { false }
+//       try {
+//         var adminArea = $.grep(results[0].address_components, function(el){
+//           return $.inArray('administrative_area_level_1', el.types) !== -1;
+//         })[0].long_name;
+//       } catch(e) { false }
+//       try {
+//         var country = $.grep(results[0].address_components, function(el){
+//           return $.inArray('country', el.types) !== -1;
+//         })[0].long_name;
+//         var countryCode = $.grep(results[0].address_components, function(el){
+//           return $.inArray('country', el.types) !== -1;
+//         })[0].short_name;
+//       } catch(e) { false }
+
+//       finalCity = postalTown ? postalTown : locality;
+//       friendlyCity = finalCity ? finalCity + ", " : "";
+//       friendlyAdminArea = adminArea ? adminArea + ", " : "";
+//       friendlyCountry = country ? country : "";;
+
+//       $('#address-computed').text(friendlyCity + friendlyAdminArea + friendlyCountry);
+//       $('#change-address').removeClass('hidden');
+//       $("#order_bill_address_attributes_country_id option[data-iso=" + countryCode +"]").attr("selected","selected") ;
+
+//       $('#order_bill_address_attributes_city').val(finalCity);
+
+//     }
+    
+//   });
+
+// }
+
+// $('#change-address').on('click', function(){
+//   $('.change-address').addClass('hidden');
+//   $('.address-hidden').removeClass('hidden');
+//   return false;
+// });
+
+// $('#order_bill_address_attributes_zipcode').on('keyup', function(){ //keyup change paste (omit tab-on keyup), autofill
+//   var currentVal = $(this).val();
+//   if (currentVal.length >= 4) {
+//     findAddress(currentVal);
+//   } else {
+//     $('#address-computed').text('');
+//     $('#order_bill_address_attributes_city').val('');
+//     $('#change-address').addClass('hidden');
+//   }
+// });
+
+// $.validator.setDefaults({
+//     highlight: function(element) {
+//       $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+//     },
+//     unhighlight: function(element) {
+//       $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+//     },
+//     errorElement: 'span',
+//     errorClass: 'help-block',
+//     errorPlacement: function(error, element) {}
+//     // errorPlacement: function(error, element) {
+//     //     if(element.parent('.input-group').length) {
+//     //         error.insertAfter(element.parent());
+//     //     } else {
+//     //         error.insertAfter(element);
+//     //     }
+//     // }
+// });
