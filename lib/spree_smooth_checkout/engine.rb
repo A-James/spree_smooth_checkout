0f2f07442ac396e89d59a4537c7d00b0ae90700c
ruslannastasiev <ruslan0607@gmail.com>
2017-06-16 15:21:58 +0300

    Add delivery step to checkout flow

--- lib/spree_smooth_checkout/engine.rb
+++ lib/spree_smooth_checkout/engine.rb
@@ -11,6 +11,12 @@ module SpreeSmoothCheckout
       g.test_framework :rspec
     end
 
+    initializer "my_gem.include_view_helpers" do |app|
+      ActiveSupport.on_load :action_view do
+        include SmoothCheckoutHelper
+      end
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
