cb66ea3e56783a803b40f3eff83583daa7b8a7d8
ruslannastasiev <ruslan0607@gmail.com>
2017-09-07 17:17:41 +0300

    Checkout displays step 2 error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Spree::Core::Engine.routes.draw do
   # A customer should never see the delivery screen,
   # but just in case, redirect back to the checkout screen.
-  get '/checkout/delivery', :to => redirect('/checkout')
 end
